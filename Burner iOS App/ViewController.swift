778107c7a9844e64164067e7e1196af202f715fc
Ayush Singh <ayush.singh21082@gmail.com>
2021-09-13 23:13:59 +0530

    Added Activity Indicator

--- Burner iOS App/ViewController.swift
+++ Burner iOS App/ViewController.swift
@@ -13,13 +13,16 @@ var movies:Array<Movie>? = []
 class ViewController: UIViewController {
     
     @IBOutlet weak var searchBox: UITextField!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     func searchMovies() {
+        showActivityIndicator()
         searchData = searchBox.text!
         let url = URL(string: "http://www.omdbapi.com/?i=\(api)&apikey=\(key)&s=\(searchData)")
         let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
@@ -33,9 +36,9 @@ class ViewController: UIViewController {
                 {
                     do
                     {
-                        //Resultado da busca do JSON
+                        //JSON search result
                         let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableLeaves) as AnyObject
-                        //Array MultiDimensões
+                        //Adding to multi dimensional array
                         if let search = myJson["Search"] as? [NSDictionary]
                         {
                             for result in search {
@@ -49,7 +52,7 @@ class ViewController: UIViewController {
                                 let auxPoster   = result["Poster"]
                                 let auxYear     = result["Year"]
                                 movies?.append(Movie(name: auxName as! String, type: auxType as! String, year: auxYear as! String, poster: auxPoster as! String))
-                            
+                            //Adding 1D Array
                                 moviesTitle.append(auxName as! String)
                                 moviesType.append(auxType as! String)
                                 moviesPoster.append(auxPoster as! String)
@@ -57,6 +60,7 @@ class ViewController: UIViewController {
                             }
                             DispatchQueue.main.async {
                                 self.performSegue(withIdentifier: "movieListing", sender: nil)
+                                self.stopActivityIndicator()
                             }
                            
                         }
@@ -74,12 +78,23 @@ class ViewController: UIViewController {
         
     }
     
+    func showActivityIndicator() {
+        activityIndicator.alpha = 1
+        activityIndicator.startAnimating()
+    }
+    func stopActivityIndicator() {
+        activityIndicator.alpha = 0
+        activityIndicator.stopAnimating()
+    }
+    
+    
 
     
     @IBAction func searchBtn(_ sender: Any) {
         searchMovies()
         
     }
+    
     
 }
 

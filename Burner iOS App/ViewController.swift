680f77cd4f7795e1f1cdc39f73ea9213f8b31f75
Ayush Singh <ayush.singh21082@gmail.com>
2021-09-13 22:29:41 +0530

    Initial Commit

---
+++ Burner iOS App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Burner iOS App
+//
+//  Created by Ayush Singh on 13/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
